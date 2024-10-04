f8ded1f4f2c7aee9b962062d780974eff77b0a41
Izabely Ferrari <belymiaferrari@gmail.com>
2024-10-03 23:47:18 -0300

    -

---
+++ ex04/index.php
@@ -0,0 +1,10 @@
+<?php 
+//TIPOS PRIMITIVOS ESCALARES
+$sobrenome = "Ferrari"; //string
+$idade = 16; //int ou integer
+$peso = 60.12; //float, double ou real(usado anteriormente)
+$casado = true; //bool
+
+
+
+
---
+++ ex04/tiposPrimitivos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos</title>
+</head>
+<body>
+    <?php 
+    $num =300;
+    echo "O valor é $num";
+    var_dump($num);
+    $num2 = (int) 3e2; //coerçao, estou ""forçando""
+    /* Tipos compostos:
+    arrays/ object
+    Tipos especiais:
+    null
+    resource
+    callabe
+    miced */
+    $vet = [6, 7,8,9]
+
+    ?>
+</body>
+</html>
